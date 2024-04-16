e163c3a69db2c2e394a08ef718761e641b8755dd
mirohi <m_hintikka@hotmail.com>
2024-04-16 14:44:26 +0300

    Add jwt token middleware and dependencies

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import "dotenv";
 
+import userRouter from "./routes/userRouter.js";
+
 const router = express.Router();
+
+router.use("/users", userRouter);
 
 export default router;
--- src/middlewares.js
+++ src/middlewares.js
@@ -3,3 +3,18 @@
 import jwt from "jsonwebtoken";
 import "dotenv/config";
 import { validationResult } from "express-validator";
+
+const authenticateToken = (req, res) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(", ")[1];
+  console.log("Token: ", token);
+  if (token == null) res.sendStatus(401);
+
+  try {
+    res.locals.user = jwt.verify(token, process.env.SECRETKEY);
+  } catch (err) {
+    res.status(403).send({ message: "Invalid token" });
+  }
+};
+
+export { authenticateToken };
