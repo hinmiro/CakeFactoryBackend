189ba46f95603630bc044121a36f7f486629ae18
mirohi <m_hintikka@hotmail.com>
2024-04-18 10:13:46 +0300

    Add jwt token middleware and dependencies

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,13 @@
 import express from "express";
 import "dotenv";
+
 import orderRouter from './routes/orderRouter.js';
 import productRouter from './routes/productRouter.js';
-
 import userRouter from "./routes/userRouter.js";
 
 const router = express.Router();
 
 router.use("/users", userRouter);
-
 router.use('/order', orderRouter);
 router.use('/product', productRouter);
 
