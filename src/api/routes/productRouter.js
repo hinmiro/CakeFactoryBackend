539a52e139bdb35535c02364f1b105d9716eb649
mirohi <m_hintikka@hotmail.com>
2024-04-19 17:41:11 +0300

    Add multer to productRouter.js

--- src/api/routes/productRouter.js
+++ src/api/routes/productRouter.js
@@ -1,9 +1,49 @@
-'use strict';
+"use strict";
 import express from "express";
-import {postProduct} from '../controllers/productController.js';
+import crypto from "crypto";
+import { postProduct } from "../controllers/productController.js";
+import multer from "multer";
+import e from "express";
+import { authToken } from "../../middlewares.js";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "/uploads");
+  },
+  filename: function (req, file, cb) {
+    const suffix = crypto.randomBytes(16).toString("hex");
+    const prefix = file.fieldname.split(".").pop();
+    let extension = "jpg";
+    if (file.mimetype === "image/png") {
+      extension = "png";
+    }
+    if (file.mimetype === "image/jpeg") {
+      extension = "jpeg";
+    }
+    const filename = `${prefix}${suffix}.${extension}`;
+    cb(null, filename);
+  },
+});
+
+const upload = multer({
+  destination: "uploads/",
+  limits: {
+    fileSize: 10 * 1024 * 2024,
+  },
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("image/")) {
+      cb(null, true);
+    } else {
+      const error = new Error("Only images are allowed");
+      error.status = 400;
+      cb(error);
+    }
+  },
+});
 
 const productRouter = express.Router();
 
-productRouter.route('/').post(postProduct);
+productRouter.route("/").post(authToken, upload.single("file"), postProduct);
 
 export default productRouter;
